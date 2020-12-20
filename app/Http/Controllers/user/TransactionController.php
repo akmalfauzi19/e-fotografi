8b7131c79e3587c11df58f27e694851e5dadce96
Hafizh Akmal Fauzi <akmal.fauzi19@gmail.com>
2020-12-20 14:22:55 +0700

    perbikan my transaction

--- app/Http/Controllers/user/TransactionController.php
+++ app/Http/Controllers/user/TransactionController.php
@@ -120,7 +120,6 @@ class TransactionController extends Controller
     public function cekStatusLogin($email)
     {
         $items = Transaction::where('email', $email)->orderBy('created_at', 'ASC')->get();
-        // $items = Transaction::with('user', 'details', 'details.user')->orderBy('id', 'DESC')->where('user_id', $id)->get();
         $menu = ProductGallery::with('product')->get();
 
         return view('user.pages.transactions.cekstatuslogin')
@@ -128,6 +127,5 @@ class TransactionController extends Controller
                 'items' => $items,
                 'menu' => $menu
             ]);
-        // return view('user.pages.transactions.cekstatus');
     }
 }
