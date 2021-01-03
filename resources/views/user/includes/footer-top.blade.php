aa6f4c0282d10373a9d7807e314d04746b3062cb
Hafizh Akmal Fauzi <akmal.fauzi19@gmail.com>
2021-01-03 23:00:05 +0700

    .

--- resources/views/user/includes/footer-top.blade.php
+++ resources/views/user/includes/footer-top.blade.php
@@ -7,40 +7,25 @@
                          <div class="aa-footer-widget">
                              <h3>Main Menu</h3>
                              <ul class="aa-footer-nav">
-                                 <li><a href="#">Home</a></li>
-                                 <li><a href="#">Our Services</a></li>
-                                 <li><a href="#">Our Products</a></li>
-                                 <li><a href="#">About Us</a></li>
-                                 <li><a href="#">Contact Us</a></li>
+                                 <li><a href="{{ route('user.home') }}">Home</a></li>
+                                 <li><a href="{{ route('user.portofolio') }}">Portofolio</a></li>
+                                 @if (Route::has('login'))
+                                     @auth
+                                         @if (Auth::user()->is_admin == 1)
+
+                                         @else
+                                             <li>
+                                                 <a href="{{ route('user.status.login', Auth::user()->email) }}">
+                                                     My Transaction
+                                                 </a>
+                                             </li>
+                                         @endif
+                                     @else
+                                         <li><a href="{{ route('user.status') }}">My Transaction</a></li>
+                                     @endauth
+                                 @endif
+                                 <li><a href="{{ route('user.contact') }}">Contact</a></li>
                              </ul>
-                         </div>
-                     </div>
-                     <div class="col-md-3 col-sm-6">
-                         <div class="aa-footer-widget">
-                             <div class="aa-footer-widget">
-                                 <h3>Knowledge Base</h3>
-                                 <ul class="aa-footer-nav">
-                                     <li><a href="#">Delivery</a></li>
-                                     <li><a href="#">Returns</a></li>
-                                     <li><a href="#">Services</a></li>
-                                     <li><a href="#">Discount</a></li>
-                                     <li><a href="#">Special Offer</a></li>
-                                 </ul>
-                             </div>
-                         </div>
-                     </div>
-                     <div class="col-md-3 col-sm-6">
-                         <div class="aa-footer-widget">
-                             <div class="aa-footer-widget">
-                                 <h3>Useful Links</h3>
-                                 <ul class="aa-footer-nav">
-                                     <li><a href="#">Site Map</a></li>
-                                     <li><a href="#">Search</a></li>
-                                     <li><a href="#">Advanced Search</a></li>
-                                     <li><a href="#">Suppliers</a></li>
-                                     <li><a href="#">FAQ</a></li>
-                                 </ul>
-                             </div>
                          </div>
                      </div>
                      <div class="col-md-3 col-sm-6">
@@ -53,15 +38,20 @@
                                      <p><span class="fa fa-envelope"></span> Aristofotografi7@gmail.com</p>
                                  </address>
                                  <div class="aa-footer-social">
-                                     <a href="https://www.facebook.com/aristofotografi/"><span
-                                             class="fa fa-facebook"></span></a>
-                                     <a href="https://www.instagram.com/aristofotografi/"><span
-                                             class="fa fa-instagram"></span></a>
+                                     <a href="https://www.facebook.com/aristofotografi/">
+                                         <span class="fa fa-facebook"></span>
+                                     </a>
+                                     <a href="https://www.instagram.com/aristofotografi/">
+                                         <span class="fa fa-instagram"></span>
+                                     </a>
                                      <a
-                                         href="https://wa.me/6287824965334?text=Halo%20AristoFotografi,%0AApakah%20paket%20foto%20tanpa%20Down%20Payment%20(DP)%20masih%20ada?%20:"><span
-                                             class="fa fa-whatsapp"></span></a>
-                                     <a href="https://www.youtube.com/channel/UCMWFpy9QnAi57BL2HvqvnoA"><span
-                                             class="fa fa-youtube"></span></a>
+                                         href="https://wa.me/6287824965334?text=Halo%20AristoFotografi,%0AApakah%20paket%20foto%20tanpa%20Down%20Payment%20(DP)%20masih%20ada?%20:">
+                                         <span class="fa fa-whatsapp">
+                                         </span>
+                                     </a>
+                                     <a href="https://www.youtube.com/channel/UCMWFpy9QnAi57BL2HvqvnoA">
+                                         <span class="fa fa-youtube"></span>
+                                     </a>
                                  </div>
                              </div>
                          </div>
